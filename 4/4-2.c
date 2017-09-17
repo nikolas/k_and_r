8c973473edc49b9a5e82325da7933032a6289f2a
Nik Nyby <nikolas@gnu.org>
2017-09-17 10:19:48 -0400

    Add atof

---
+++ 4/4-2.c
@@ -0,0 +1,35 @@
+#include <ctype.h>
+#include <stdio.h>
+
+double atof(char s[]) {
+    double val, power;
+    int i, sign;
+
+    for (i = 0; isspace(s[i]); i++)
+        ;
+
+    sign = (s[i] == '-') ? -1 : 1;
+
+    if (s[i] == '+' || s[i] == '-') {
+        i++;
+    }
+
+    for (val = 0.0; isdigit(s[i]); i++) {
+        val = 10.0 * val + (s[i] - '0');
+    }
+    if (s[i] == '.') {
+        i++;
+    }
+    for (power = 1.0; isdigit(s[i]); i++) {
+        val = 10.0 * val + (s[i] - '0');
+        power *= 10.0;
+    }
+
+    return sign * val / power;
+}
+
+int main() {
+    char s[] = "56.1412";
+    printf("atof(%s): %f\n", s, atof(s));
+    printf("* 2 =: %f\n", atof(s) * 2);
+}
