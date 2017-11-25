e0d0e5ffdd49342e4ee906c27b63cdd1c8fe032e
Nik Nyby <nikolas@gnu.org>
2017-11-25 17:38:54 -0500

    add strncpy

--- 5/5-5.c
+++ 5/5-5.c
@@ -1,6 +1,29 @@
 #include <stdio.h>
 
+void mystrncpy(char *dest, char *src, int n) {
+    int i, x;
+
+    i = 0;
+    while ((*(dest + i) = *(src + i)) != '\0' && i < (n - 1)) {
+        i++;
+    }
+}
+
 void mystrncat(char *s, char *t, int n) {
+    int i, j, x;
+
+    i = j = 0;
+    while (*(s + i) != '\0') {
+        i++;
+    }
+
+    x = 1;
+    while ((*(s + i++) = *(t + j++)) != '\0' && x < n) {
+        x++;
+    }
+}
+
+void mystrncmp(char *s, char *t, int n) {
     int i, j, x;
 
     i = j = 0;
@@ -18,6 +41,6 @@ int main() {
     char a[100] = "abc";
     char b[100] = "defghijklmnop";
 
-    mystrncat(a, b, 5);
-    printf("strncat: %s, %s\n", a, b);
+    mystrncpy(a, b, 50);
+    printf("strncpy: %s, %s\n", a, b);
 }
